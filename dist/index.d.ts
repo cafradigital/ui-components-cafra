02a219fe579a61d5e587fcdd6caeeaef326e71d4
Thiago Almeida Amorim <thiago2332003@gmail.com>
2023-06-29 15:12:59 -0300

    Removendo a Pasta DIST

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { InputHTMLAttributes } from 'react';
-
-interface IOwnProps extends InputHTMLAttributes<HTMLButtonElement> {
-}
-declare const ButtonWrapper: React.FC<IOwnProps>;
-
-export { ButtonWrapper as UIButton };
